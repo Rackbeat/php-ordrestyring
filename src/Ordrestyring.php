fdd5d18a47c62e0fb59465a1c5030d1ab42ce120
Lasse Rafn <lasserafn@gmail.com>
2017-01-24 17:15:26 +0100

    Cleanup

--- src/Ordrestyring.php
+++ src/Ordrestyring.php
@@ -8,40 +8,101 @@ class Ordrestyring
 	/** @var Client */
 	protected $client;
 
-	public function __construct(string $apikey = '')
+	public function __construct( string $apikey = '' )
 	{
-		$this->client = new Client([
+		$this->client = new Client( [
 			'base_uri' => 'https://v1.api.ordrestyring.dk', // Yes, Ordrestyring defaults to http in their docs, but https has a valid certificate.
-		    'headers' => [
-		    	'Authorization' => [
-		    		"{$apikey}:x", 'Password' // Ordrestyring does not care about the password field in Auth, I simply default it to 'Password'
-			    ]
-		    ]
-		]);
+			'headers'  => [
+				'Authorization' => [
+					"{$apikey}:x",
+					''
+				]
+			]
+		] );
 	}
 
-	public function debtors() : DebtorRequest {
-		return new DebtorRequest($this->client);
+	public function debtors(): DebtorRequest
+	{
+		return new DebtorRequest( $this->client );
 	}
 
-	public function debtorInvoices(){}
-	public function creditors(){}
-	public function creditorInvoices(){}
-	public function gps(){}
-	public function internalGoods(){}
-	public function paymentTerms(){}
-	public function debtorCategories(){}
-	public function accountPlan(){}
-	public function vatTypes(){}
-	public function departments(){}
-	public function cases(){}
-	public function caseStatuses(){}
-	public function caseTypes(){}
-	public function caseStatusHistory(){}
-	public function caseMaterials(){}
-	public function caseDocumentation(){}
-	public function hours(){}
-	public function employeeTypes(){}
-	public function calendar(){}
-	public function user(){}
+	public function debtorInvoices()
+	{
+	}
+
+	public function creditors()
+	{
+	}
+
+	public function creditorInvoices()
+	{
+	}
+
+	public function gps()
+	{
+	}
+
+	public function internalGoods()
+	{
+	}
+
+	public function paymentTerms()
+	{
+	}
+
+	public function debtorCategories()
+	{
+	}
+
+	public function accountPlan()
+	{
+	}
+
+	public function vatTypes()
+	{
+	}
+
+	public function departments()
+	{
+	}
+
+	public function cases()
+	{
+	}
+
+	public function caseStatuses()
+	{
+	}
+
+	public function caseTypes()
+	{
+	}
+
+	public function caseStatusHistory()
+	{
+	}
+
+	public function caseMaterials()
+	{
+	}
+
+	public function caseDocumentation()
+	{
+	}
+
+	public function hours()
+	{
+	}
+
+	public function employeeTypes()
+	{
+	}
+
+	public function calendar()
+	{
+	}
+
+	public function user()
+	{
+	}
 }
