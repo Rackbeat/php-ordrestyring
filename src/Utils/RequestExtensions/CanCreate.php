edd7c50ac6180145873e032a41bf208b348ab0f3
Stefan Ninic <ninicstefan94@gmail.com>
2020-05-13 14:25:41 +0200

    Return create and delete response

--- src/Utils/RequestExtensions/CanCreate.php
+++ src/Utils/RequestExtensions/CanCreate.php
@@ -4,12 +4,11 @@ namespace LasseRafn\Ordrestyring\Utils\RequestExtensions;
 
 trait CanCreate
 {
-    public function create($data)
-    {
-        $response = $this->getResponse(function () use ($data) {
-            return $this->client->post("{$this->endpoint}", [
-                'json' => $data,
-            ]);
-        });
-    }
+	public function create( $data ) {
+		return $this->getResponse( function () use ( $data ) {
+			return $this->client->post( "{$this->endpoint}", [
+				'json' => $data,
+			] );
+		} );
+	}
 }
--- src/Utils/RequestExtensions/CanDelete.php
+++ src/Utils/RequestExtensions/CanDelete.php
@@ -6,7 +6,7 @@ trait CanDelete
 {
     public function delete($primaryKey)
     {
-        $response = $this->getResponse(function () use ($primaryKey) {
+        return $this->getResponse(function () use ($primaryKey) {
             return $this->client->delete("{$this->endpoint}/{$primaryKey}");
         });
     }
